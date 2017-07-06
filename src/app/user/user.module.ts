b80b41e14f098c50680e9642a120dc542c8b9d9d
Alek Aleksanyan <alek.aleksanyan@arm.synisys.com>
2017-07-06 17:37:58 +0400

    commit from work

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import {CommonModule} from "@angular/common";
 import {RouterModule} from "@angular/router";
-import {FormsModule} from "@angular/forms"
+import {FormsModule, ReactiveFormsModule} from "@angular/forms"
+
 import {ProfileComponent} from "./profile.component";
 import {userRoutes} from "./user.routes";
 import {LoginComponent} from "app/user/login.component";
@@ -10,7 +11,8 @@ import {LoginComponent} from "app/user/login.component";
   imports:[
     CommonModule,
     RouterModule.forChild(userRoutes),
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   declarations: [
     ProfileComponent,
